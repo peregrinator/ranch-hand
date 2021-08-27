1198bd6a1ec03aabafb593771eea2537eb28d0ef
Peregrinator <bob.burbach@gmail.com>
2021-08-26 20:35:04 -0700

    Add Oh My Zsh shortcuts and command to install them

---
+++ files/ranch-hand.plugin.zsh
@@ -0,0 +1,4 @@
+alias rh='ranch-hand'         # main binary alias
+alias rhs='ranch-hand setup'  # setup ranch hand with Rancher CLI credentials
+alias rhi='ranch-hand init'   # initialize default ranch hand settings in a project
+alias rhe='ranch-hand exec'   # run a command in a container
--- lib/ranch_hand/base.rb
+++ lib/ranch_hand/base.rb
@@ -12,5 +12,27 @@ module RanchHand
       RanchHand::Config.create
       RanchHand::Logger.info("initialization complete")
     end
+
+    def self.install_ohmyzsh
+      RanchHand::Logger.info("installing ranch-hand Oh My Zsh shortcuts")
+
+      unless ENV['ZSH_CUSTOM'] || ENV['ZSH']
+        RanchHand::Logger.warn("Could not find $ZSH_CUSTOM or $ZSH in your environment, can not install shortcuts")
+        return
+      end
+
+      zsh_custom_dir = ENV['ZSH_CUSTOM'] || File.join(ENV['ZSH'], 'custom')
+
+      zsh_plugin_dir = File.join(zsh_custom_dir, 'plugins', 'ranch-hand')
+      FileUtils.mkdir_p(zsh_plugin_dir)
+
+      current_path = File.expand_path(File.dirname(__FILE__))
+      gem_plugin_path = File.join(current_path, '..', '..', 'files', 'ranch-hand.plugin.zsh')
+
+      FileUtils.cp(gem_plugin_path, zsh_plugin_dir)
+
+      RanchHand::Logger.info("installation complete")
+      RanchHand::Logger.info("Don't forget to add 'ranch-hand' to your plugins in ~/.zshrc -- e.g. plugins=(ranch-hand) -- and then 'source ~/.zshrc'")
+    end
   end
 end
