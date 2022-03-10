b8f94b2108bcfd2ab94d4cab9a14f7a3794ad6f1
Peregrinator <bob.burbach@gmail.com>
2022-03-10 14:43:45 -0800

    Update zsh plugin aliases

--- files/ranch-hand.plugin.zsh
+++ files/ranch-hand.plugin.zsh
@@ -1,4 +1,6 @@
-alias rh='ranch-hand'         # main binary alias
-alias rhs='ranch-hand setup'  # setup ranch hand with Rancher CLI credentials
-alias rhi='ranch-hand init'   # initialize default ranch hand settings in a project
-alias rhe='ranch-hand exec'   # run a command in a container
+alias rh='ranch-hand'                       # main binary alias
+alias rhs='ranch-hand setup'                # setup ranch hand with Rancher CLI credentials
+alias rhi='ranch-hand init'                 # initialize default ranch hand settings in a project
+alias rhu='ranch-hand update_config'        # update default ranch hand settings for a project
+alias rhe='ranch-hand exec'                 # run a command in a container
+alias rhes='ranch-hand exec --skip-prefix'  # run a command in a container but skip default command_prefix
